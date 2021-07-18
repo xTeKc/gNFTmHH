4041f4f28bf2ed6e6d4d68944e418ad6055d09d0
xTeKc <bcd3v@pm.me>
2021-07-18 09:41:29 -0400

    add imports

---
+++ src/store/interactions.tsx
@@ -0,0 +1,12 @@
+
+
+import Web3 from 'web3'
+import {
+  web3Loaded,
+  contractLoaded,
+  web3NetworkLoaded,
+  web3AccountLoaded,
+  web3BalanceLoaded,
+  metadataLoaded,
+  nftStateLoaded
+} from './actions'
