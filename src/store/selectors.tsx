3e217291cf1f6283dbdf3775c41167e0271d3b8d
xTeKc <bcd3v@pm.me>
2021-07-18 09:33:28 -0400

    add web3

--- src/store/selectors.tsx
+++ src/store/selectors.tsx
@@ -1,3 +1,5 @@
 import { createSelector } from 'reselect'
 import { get } from 'lodash'
 
+const web3 = state => get(state, 'web3.connection')
+export const web3Selector = createSelector(web3, w => w)
