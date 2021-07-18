8f97aeebc6c034f14cda4f54573776027b95674b
xTeKc <bcd3v@pm.me>
2021-07-18 07:59:12 -0400

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,15 @@
-import { task } from "hardhat/config";
+//import { task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (args, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
