7c6654ecb2d1e109940a2606b2ec1dc3a2c3324d
xTeKc <bcd3v@pm.me>
2021-07-18 09:42:23 -0400

    export loadweb3

--- src/store/interactions.tsx
+++ src/store/interactions.tsx
@@ -10,3 +10,18 @@ import {
   metadataLoaded,
   nftStateLoaded
 } from './actions'
+
+export const loadWeb3 = async (dispatch) => {
+    try{
+      if(typeof window.ethereum!=='undefined'){
+        window.ethereum.autoRefreshOnNetworkChange = false;
+        const web3 = new Web3(window.ethereum)
+        dispatch(web3Loaded(web3))
+        return web3
+      }
+    } catch (e) {
+      console.log('Error, load Web3: ', e)
+    }
+  }
+
+  
