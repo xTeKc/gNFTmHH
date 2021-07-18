16b940d938b4074b6a14cddd4706c5cfe799c240
xTeKc <bcd3v@pm.me>
2021-07-18 09:35:03 -0400

    add balance

--- src/store/selectors.tsx
+++ src/store/selectors.tsx
@@ -9,3 +9,7 @@ export const networkSelector = createSelector(network, n => n)
 
 const account = state => get(state, 'web3.account')
 export const accountSelector = createSelector(account, a => a)
+
+const balance = state => get(state, 'web3.balance', 0)
+export const balanceSelector = createSelector(balance, e => e)
+
