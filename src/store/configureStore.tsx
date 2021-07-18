c40506a728fcb51601d28e97293d5281aa4ae2ee
xTeKc <bcd3v@pm.me>
2021-07-18 09:52:58 -0400

    add content for redux dev tools

--- src/store/configureStore.tsx
+++ src/store/configureStore.tsx
@@ -5,3 +5,13 @@ import rootReducer from './reducers'
 const loggerMiddleware = createLogger()
 const middleware = []
 
+// For Redux Dev Tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export default function configureStore(preloadedState) {
+  return createStore(
+    rootReducer,
+    preloadedState,
+    composeEnhancers(applyMiddleware(...middleware, loggerMiddleware))
+  )
+}
