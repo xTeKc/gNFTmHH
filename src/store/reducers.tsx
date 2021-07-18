11b179518ce9b0ca5671762fe99dd6516828de33
xTeKc <bcd3v@pm.me>
2021-07-18 09:37:58 -0400

    add web3 func

--- src/store/reducers.tsx
+++ src/store/reducers.tsx
@@ -1,2 +1,18 @@
 import { combineReducers } from 'redux';
 
+function web3(state = {}, action) {
+    switch (action.type) {
+      case 'WEB3_LOADED':
+        return { ...state, connection: action.connection }
+      case 'WEB3_NETWORK_LOADED':
+        return { ...state, network: action.network }
+      case 'WEB3_ACCOUNT_LOADED':
+        return { ...state, account: action.account }
+      case 'WEB3_BALANCE_LOADED':
+        return { ...state, balance: action.balance }
+      default:
+        return state
+    }
+  }
+
+  
