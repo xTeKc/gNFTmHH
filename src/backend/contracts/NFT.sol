cfed157e7593bffbb9b7d0a785875f28c05eea6f
xTeKc <bcd3v@pm.me>
2021-07-18 11:14:33 -0400

    refactor

--- src/backend/contracts/NFT.sol
+++ src/backend/contracts/NFT.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
+contract NFT is ERC721, Ownable {
+  address payable public _owner;
 
 }
