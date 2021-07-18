12481e70ebebc86eb79bebd9ce720af839833a5c
xTeKc <bcd3v@pm.me>
2021-07-18 09:25:37 -0400

    add imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
-import App from './App';
+import App from './components/App';
+import configureStore from './store/configureStore' 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
